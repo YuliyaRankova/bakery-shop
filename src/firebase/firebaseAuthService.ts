9049d4c693134c32541b7881bcb3095c87e6a510
YuliyaRankova <nikypod@gmail.com>
2025-06-26 12:02:21 +0300

    firestore activated. start to create DB service

--- src/firebase/firebaseAuthService.ts
+++ src/firebase/firebaseAuthService.ts
@@ -1,6 +1,6 @@
 import {signOut, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword} from "firebase/auth"
 import type {LoginData} from "../utils/shop-types.ts";
-import {auth} from "../configurations/firebase.ts";
+import {auth} from "../configurations/firebase-config.ts";
 
 const loginWithEmail = async (data: LoginData) => {
     await signInWithEmailAndPassword(auth, data.email, data.password);
--- src/utils/shop-types.ts
+++ src/utils/shop-types.ts
@@ -34,3 +34,12 @@ export type SignUpData = {
     email:string,
     password:string
 };
+
+export type ProductType ={
+    id?:string,
+    title:string,
+    category:string,
+    unit:string,
+    cost:number,
+    img:string
+}
